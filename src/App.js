c057d84846a381144a845ca4b47d505cd8291838
davidyang2149 <davidyang2149@gmail.com>
2020-09-13 20:17:28 +0900

    [Add]collaborative-editor-sample draft.js Editor Add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import RichTextEditor from "./components/RichTextEditor";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <RichTextEditor />
+    </div>
+  );
 }
 
 export default App;
